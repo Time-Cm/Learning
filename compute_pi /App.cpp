533cd8e801dc865c224c7028537b59b45d1008dd
Time-Cm <90051981+Time-Cm@users.noreply.github.com>
2022-03-26 00:26:10 +0800

    Update and rename compute_pi/App.cpp to compute_pi /App.cpp

--- compute_pi /App.cpp
+++ compute_pi /App.cpp
@@ -5,8 +5,8 @@ double App::pi(App *running)
     using namespace std;
 
     double result = 0;
-    int inCount = 0;         //命中总次数(The number of hitting)
-    int *data[THREADS] = {}; //用于线程数据交流的内存块(It is used to swap data between thread)
+    int inCount = 0;         //命中总次数
+    int *data[THREADS] = {}; //用于线程数据交流的内存块
     thread *thPtr[THREADS] = {};
 
     for (int i = 0; i < THREADS; i++)
@@ -41,7 +41,7 @@ void App::compute(int *inputData)
     using namespace std;
 
     int &inCount = *inputData;
-    int threadsIndex = inCount; //保存传入的数据块中包含的线程信息(Save information of this thread)
+    int threadsIndex = inCount; //保存传入的数据块中包含的线程信息
     inCount = 0;
 
     srand(time(nullptr) / (threadsIndex + 1));
